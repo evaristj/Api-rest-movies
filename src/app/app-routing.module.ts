ddb254754ddc41be9a12d17091dbcd2b977b7ad2
Evarist Jaume <evaristjaumet@gmail.com>
2019-01-15 15:27:27 +0100

    create components show movies and single movie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPageComponent } from './main-page/main-page.component';
+import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: MainPageComponent },
+  { path: 'movie/:id', component: MovieDetailComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', redirectTo: 'home' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
