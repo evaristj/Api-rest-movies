2f0d3b86c6df64cba33fb536aee254b6b1ecfce7
Evarist Jaume <evaristjaumet@gmail.com>
2019-01-16 16:38:29 +0100

    created component for search single movie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { MainPageComponent } from './main-page/main-page.component';
 import { MovieDetailComponent } from './movie-detail/movie-detail.component';
 import { MoviesService } from './movies.service';
 import { HttpClientModule } from '@angular/common/http';
+import { SearchSingleMovieComponent } from './search-single-movie/search-single-movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
-    MovieDetailComponent
+    MovieDetailComponent,
+    SearchSingleMovieComponent
   ],
   imports: [
     BrowserModule,
--- src/app/movie-detail/movie-detail.component.ts
+++ src/app/movie-detail/movie-detail.component.ts
@@ -12,13 +12,17 @@ export class MovieDetailComponent implements OnInit {
   id: number;
   singleMovie: object;
   getImgDomain = 'https://image.tmdb.org/t/p/w185_and_h278_bestv2/';
+  loading = false;
 
   constructor(private route: ActivatedRoute, private movieService: MoviesService) { }
 
   ngOnInit() {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.loading = true;
     this.movieService.getSingleMovie(this.id).then(data => {
       this.singleMovie = data;
+      this.loading = false;
+
     });
   }
 
